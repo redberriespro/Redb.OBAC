7c80a2adbaa235f85ffa378940763a622402b156
Yury <yury@redberries.pro>
2021-11-25 17:31:27 +0300

    Update Program.cs

--- Examples/HelloObacEf/Program.cs
+++ Examples/HelloObacEf/Program.cs
@@ -30,14 +30,15 @@ namespace HelloObac
         public static async Task Main()
         {
             // initialize local DB
-            ctx = new HelloDbContext();
+            ctx = new HelloDbContext(); // the context must inherit ObacEpContextBase to be able to receive EP messages.
             await ctx.Database.EnsureCreatedAsync();
             
             // configure OBAC
             var pgStorage = new PgSqlObacStorageProvider(OBAC_CONNECTION);
             await pgStorage.EnsureDatabaseExists();
             
-            var epHouseReceiver = new EffectivePermissionsEfReceiver(ctx);
+            // NOTE the context instance passed to the receiver could not be used across other program when in production code
+            var epHouseReceiver = new EffectivePermissionsEfReceiver(ctx); 
             
             // initialize OBAC with out effective permission's receiver
             var obacConfiguration = ObacManager.CreateConfiguration(pgStorage, epHouseReceiver);
