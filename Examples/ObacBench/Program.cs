87f22f34065c3dddfaa1ecac60eace1b0bb9f3df
Yury Skaletskiy <yury@redberries.pro>
2022-04-28 13:21:58 +0200

    7

--- Examples/ObacBench/Program.cs
+++ Examples/ObacBench/Program.cs
@@ -25,4 +25,4 @@ var obacConfiguration = ObacManager.CreateConfiguration(pgStorage, epHouseReceiv
 
 var obacManager = obacConfiguration.GetObjectManager();
 
-// todo implement 6
+// todo implement 7
