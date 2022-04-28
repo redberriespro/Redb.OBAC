6a07c25a9b47f878b7f1ec81a90cf144af473db1
Yury Skaletskiy <yury@redberries.pro>
2022-04-28 11:30:47 +0200

    net5 obac test

---
+++ Examples/ObacBench/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HelloObac;
+using Redb.OBAC;
+using Redb.OBAC.Client.EffectivePermissionsReceiver;
+using Redb.OBAC.PgSql;
+
+Console.WriteLine("Bench Test");
+
+const string OBAC_CONNECTION =
+    "Host=localhost;Port=5432;Database=obac;Username=postgres;Password=12345678";
+
+// initialize local DB
+var ctx = new HelloDbContext(); // the context must inherit ObacEpContextBase to be able to receive EP messages.
+await ctx.Database.EnsureCreatedAsync();
+            
+// configure OBAC
+var pgStorage = new PgSqlObacStorageProvider(OBAC_CONNECTION);
+await pgStorage.EnsureDatabaseExists();
+            
+// NOTE the context instance passed to the receiver could not be used across other program when in production code
+var epHouseReceiver = new EffectivePermissionsEfReceiver(ctx); 
+            
+// initialize OBAC with out effective permission's receiver
+var obacConfiguration = ObacManager.CreateConfiguration(pgStorage, epHouseReceiver);
+
+var obacManager = obacConfiguration.GetObjectManager();
+
+// todo
